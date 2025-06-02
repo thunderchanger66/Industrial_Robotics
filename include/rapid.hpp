dd6cc24a0563b95e95128b05bfa220153c83a91b
thunderchanger66 <thunderchanger@outlook.com>
2025-06-02 21:06:12 +0800

    changepoint

--- include/rapid.hpp
+++ include/rapid.hpp
@@ -11,14 +11,12 @@ private:
     float base_y;
     float base_z;
     std::ofstream outfile;// 文件操作的对象，要输出到path.txt；这个是target点
-    std::ofstream rapidcode;// 这个是MOVE的操作
 
 public:
     rapid(float scale = 0.5f, float base_x = 1263.7f, float base_y = 0.0f, float base_z = 938.01f): 
         scale(scale), base_x(base_x), base_y(base_y), base_z(base_z), 
-        outfile("E:\\STUDY\\Junior.down\\Industrial_Robotics\\output\\path.txt")/*,
-        rapidcode("E:\\STUDY\\Junior.down\\Industrial_Robotics\\output\\rapidcode.txt")*/ {}
-    ~rapid() { outfile.close(); /*rapidcode.close();*/ }
+        outfile("E:\\STUDY\\Junior.down\\Industrial_Robotics\\output\\path.txt") {}
+    ~rapid() { outfile.close(); }
     void toTXT();
     void set_scale(float num) { scale = num; }
     void set_base(float num1, float num2, float num3) { base_x = num1; base_y = num2; base_x = num3; }
--- src/rapid.cpp
+++ src/rapid.cpp
@@ -6,28 +6,24 @@
 void rapid::toTXT()
 {
     std::vector<std::vector<cv::Point>> theContours = gettheCoutours(1);// 获得采样后的点
-
-    int index = 1;
-    outfile << "CONST robtarget myPath{" << getpointNum() << "} := [\n";
+    
     for(size_t i = 0; i < theContours.size(); i++)
     {
+        int index = 0;
+        outfile << "CONST robtarget myPath" << i << "{" << theContours[i].size() << "} := [\n";
         for(size_t j = 0; j < theContours[i].size(); j++)
         {
             float wx = base_x + scale * theContours[i][j].x;
             float wy = base_y + scale * theContours[i][j].y;
             float wz = base_z;// Z坐标不变
 
-            // outfile << "CONST robtarget p" << index << " := [["
-            //     << wx << "," << wy << "," << wz << "],[1,0,0,0],[0,0,0,0],[9E9,9E9,9E9,9E9,9E9,9E9]];\n";
             outfile << "[[" << wx << "," << wy << "," << wz
                 << "],[0.00139266,-0.491072,0.870802,-0.0234724],[0,-1,-1,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]]";
-            if(index != getpointNum())
+            if(index != theContours[i].size() - 1)
                 outfile << ",\n";
-
-            //rapidcode << "MoveL p" << index << ", v100, z5, AW_Gun;\n";// 修改后使用循环调用MoveL，不使用这个
 
             index++;
         }
+        outfile << "\n];\n";
     }
-    outfile << "\n];\n";
 }
